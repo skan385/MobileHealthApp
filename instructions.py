9b4faadc70aa8b63cf9ff7990569ffe29fca8e81
iskander <68424995+skan385@users.noreply.github.com>
2021-06-17 16:33:56 +0600

    Add files via upload

---
+++ instructions.py
@@ -0,0 +1,22 @@
+# Модуль для хранения текстов всех инструкций
+
+# Здесь должен быть твой код
+
+txt_instruction = '''this helps you test your health. \n
+tests your heart under physical pressure. \n
+it tests your heart rate in 15 sec \n
+30 sit-ups in 45 sec. \n
+tests your heart again during the excersize and 30 sec after the excerize \n '''
+
+txt_test1 = ''' measure your heart rate in 15 sec \n
+write it down in the right spot'''
+
+txt_test2 = '''do 30 sit-ups in 45 sec \n
+press "begin" to start the counter \n
+do sit ups like it shows in the counter '''
+
+txt_test3 = '''measure your heart rate twice in a minute \n
+once in the first 15 sec and again in the last 15 minutes  \n
+write the results in the correct spot'''
+
+txt_sits = 'do 30 sit ups in 45 sec'
